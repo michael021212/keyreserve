b6839b52b04699893676bbdc29d95c3afd6ccb12
jiapinggit <jumpping4@gmail.com>
2018-02-09 13:58:02 +0900

    パスワードリマインダ

---
+++ app/mailers/users/user_mailer.rb
@@ -0,0 +1,9 @@
+class Users::UserMailer < ApplicationMailer
+  def reset_password_email(user)
+    @user = User.find user.id
+    # @url  = edit_password_reset_url(@user.reset_password_token)
+    mail(to: user.email,
+         from: 'support@key-reserve.com',
+         subject: '【KEY RESERVE】パスワード再発行のお知らせ')
+  end
+end
