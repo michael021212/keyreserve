ab3955794f4c8b639153646ccfcb439963989324
hisaju <hisaju@gmail.com>
2018-03-25 15:36:43 +0900

    不具合修正

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -33,12 +33,12 @@ Rails.application.config.sorcery.configure do |config|
   # How long in seconds to keep the session alive.
   # Default: `3600`
   #
-  # config.session_timeout =
+  config.session_timeout = 1814400
 
   # Use the last action as the beginning of session timeout.
   # Default: `false`
   #
-  # config.session_timeout_from_last_action =
+  config.session_timeout_from_last_action = true
 
   # -- http_basic_auth --
   # What realm to display for which controller name. For example {"My App" => "Application"}
