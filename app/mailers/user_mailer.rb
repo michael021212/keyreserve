e2adbb9d7bf5813df0a5357dd61bc986c8e54ac9
hisaju <hisaju@gmail.com>
2018-04-06 17:00:08 +0900

    メールテンプレートの整理

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def event_informing_mail(information, user)
     @information = information
     @user = user
-    mail(to: @user.email ,subject: "【#{@information.info_type_i18n}】 #{@information.title}")
+    mail(to: @user.email ,subject: "#{@information.title} - #{@information.shop_name}")
   end
 end
