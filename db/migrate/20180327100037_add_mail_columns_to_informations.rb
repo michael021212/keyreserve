8b34da3382a0f2f06e100f87442cc4f4b4068287
hisaju <hisaju@gmail.com>
2018-04-06 16:30:08 +0900

    デフォルト値を入れないとエラーになるので修正

--- db/migrate/20180327100037_add_mail_columns_to_informations.rb
+++ db/migrate/20180327100037_add_mail_columns_to_informations.rb
@@ -1,7 +1,7 @@
 class AddMailColumnsToInformations < ActiveRecord::Migration[5.1]
   def change
     change_column :information, :shop_id, :integer, null: true, after: :id
-    add_column :information, :publish_time, :datetime, null: false, after: :description
+    add_column :information, :publish_time, :datetime, null: false, default: '2018-01-01', after: :description
     add_column :information, :mail_send_flag, :boolean, default: false, after: :publish_time
     add_column :information, :info_type, :integer, default: 1, null: false, after: :mail_send_flag
   end
