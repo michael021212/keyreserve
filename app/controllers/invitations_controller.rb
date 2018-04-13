2d8599a27e3640c842d767c3746e49e60d8485ed
jiapinggit <jumpping4@gmail.com>
2018-04-13 20:48:11 +0900

    update!なので、rescueを追加する

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -10,7 +10,7 @@ class InvitationsController <  ApplicationController
   def new; end
 
   def create
-    current_user.update(parent_token: Digest::MD5.hexdigest(current_user.email)) if current_user.parent_token.nil?
+    current_user.update(parent_token: Digest::MD5.hexdigest(current_user.email))  if current_user.parent_token.nil?
     if params[:email].present?
       NotificationMailer.user_invitation(params[:email], current_user).deliver_now
       redirect_to invitations_path, notice: 'ご招待メールを送りました'
@@ -18,6 +18,9 @@ class InvitationsController <  ApplicationController
       flash[:alert] = 'メールアドレスを入力してください。'
       render :new
     end
+  rescue => e
+    logger.error e.message
+    render :new
   end
 
   private
