f0404f8c5dce8284f318003d5cbbed0223184983
hisaju <hisaju@gmail.com>
2018-04-15 11:14:04 +0900

    表示変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403062150) do
+ActiveRecord::Schema.define(version: 20180407054952) do
 
   create_table "admin_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
@@ -99,7 +99,17 @@ ActiveRecord::Schema.define(version: 20180403062150) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.integer "info_target_type", default: 2, null: false
     t.index ["shop_id"], name: "index_information_on_shop_id"
+  end
+
+  create_table "information_shops", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "information_id", null: false
+    t.bigint "shop_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["information_id"], name: "index_information_shops_on_information_id"
+    t.index ["shop_id"], name: "index_information_shops_on_shop_id"
   end
 
   create_table "payments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
