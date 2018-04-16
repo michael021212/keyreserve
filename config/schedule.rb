8ade2797477cec4923f820d095748abaec756045
hisaju <hisaju@gmail.com>
2018-04-16 16:10:23 +0900

    ログのパス修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,7 @@
-set :output, "/path/to/my/cron_log.log"
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+rails_env = ENV['RAILS_ENV'] || :development
+set :environment, rails_env
+set :output, "#{Rails.root}/log/cron.log"
 
 every 5.minutes do
   rake 'information:informing_mail'
