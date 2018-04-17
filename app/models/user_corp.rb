31deea2cd298d6b66dff78cbd6900fe82e662931
jiapinggit <jumpping4@gmail.com>
2018-04-17 16:50:00 +0900

    remove_parent_idを追加

--- app/models/user_corp.rb
+++ app/models/user_corp.rb
@@ -6,6 +6,8 @@ class UserCorp < User
 
   before_validation :set_corp_settings
 
+  before_destroy :remove_parent_id
+
   def set_corp_settings
     self.parent_id = nil
     pass = SecureRandom.hex(10)
@@ -14,5 +16,11 @@ class UserCorp < User
     self.email = SecureRandom.hex(10) + '@key-stations.jp'
     self.advertise_notice_flag = false
     self.user_type = :parent_corporation
+  end
+
+  def remove_parent_id
+    users.each do |user|
+      user.update(parent_id: nil)
+    end
   end
 end
