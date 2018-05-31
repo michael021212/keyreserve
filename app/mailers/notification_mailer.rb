1b0a1b8f1510e5a98200a2535f586e2b94ceb643
jiapinggit <jumpping4@gmail.com>
2018-05-31 15:57:26 +0900

    添付ファイル修正

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -29,6 +29,7 @@ class NotificationMailer < ApplicationMailer
     @not_member_ftp = reservation.facility.facility_temporary_plans.where.not(standard_price_per_hour: 0).where(plan_id: nil)
     @ftp = @member_ftps.present? ? @member_ftps.first : @not_member_ftp.first
     @ftp_title = @ftp.guide_mail_title.present? ? @ftp.guide_mail_title : 'この度は、KEY STATION OFFICE会議室のご予約誠にありがとうございます'
+    attachments[@ftp.guide_file.file.filename] = @ftp.guide_file.read if @ftp.guide_file.present?
     mail(to: reservation.user.email, subject: @ftp_title)
   end
 end
