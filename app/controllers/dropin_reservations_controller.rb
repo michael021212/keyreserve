2c999492bb172f913e5369157e13bca76b92e401
jiapinggit <jumpping4@gmail.com>
2018-07-06 10:55:52 +0900

    本人確認エラーの追加

--- app/controllers/dropin_reservations_controller.rb
+++ app/controllers/dropin_reservations_controller.rb
@@ -48,6 +48,10 @@ class DropinReservationsController <  ApplicationController
   def confirm
     session[:dropin_spot] = params[:dropin_spot] if params[:dropin_spot].present?
     @facility = Facility.find(session[:dropin_spot]['facility_id'])
+    if current_user.personal_identification.nil?
+      flash[:error] = "こちらの施設は本人確認済みの利用者のみご利用できます。本人確認の登録は#{view_context.link_to('こちら', user_path)}です".html_safe
+      return render :new
+    end
 
     if session[:dropin_spot]['sub_plan'].blank?
       flash[:error] = 'ご利用プランを入力してください'
