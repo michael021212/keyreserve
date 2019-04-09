a017f40aea8269c121bf0af66cccabf8b3012743
NaokiMochizuki <naoki.mochziuki1006@gmail.com>
2019-04-09 21:27:57 +0900

    フォームから施設を追加できるように

--- app/controllers/admin/facilities_controller.rb
+++ app/controllers/admin/facilities_controller.rb
@@ -55,7 +55,7 @@ class Admin::FacilitiesController < AdminController
 
   def facility_params
     params.require(:facility).permit(
-      :shop_id, :name, :image, :description, :max_num, :facility_type,
+      :shop_id, :name, :image, :description, :max_num, :facility_type, :address, :lat, :lon,
       facility_plans_attributes: [:id, :plan_id, '_destroy']
     )
   end
