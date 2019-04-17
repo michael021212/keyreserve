829fd2faf2a1e4f6df1a1d5127bda4bcc20d9a1c
NaokiMochizuki <naoki.mochziuki1006@gmail.com>
2019-04-17 12:40:47 +0900

    検索時の賃貸物件店舗名は、全て統一してKEY STATION 賃貸物件に

---
+++ app/decorators/facility_decorator.rb
@@ -0,0 +1,8 @@
+class FacilityDecorator < Draper::Decorator
+  delegate_all
+
+  def display_name
+    rent? ? "#{name} - #{Shop.find_by(id: Facility::RENT_SHOP_ID).name}" : "#{name} - #{shop.name}"
+  end
+
+end
