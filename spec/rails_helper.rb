c0fc1ff1ff92f878689ccddd1086faa924ae28ff
williansaio <williansaio23@gmail.com>
2019-04-23 17:09:31 +0900

    [ADD]shops_specの追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,6 +24,7 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
   config.include SessionMacros, type: :feature
+  config.include JsHelper, type: :feature
   # ロードするfixtureのパスを指定
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
