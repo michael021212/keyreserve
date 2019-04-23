c791b2f1b2948014ef6341027aaebff63cc4d1e1
williansaio <williansaio23@gmail.com>
2019-04-23 17:09:54 +0900

    [ADD]VCRの設定とファイルの追加

--- spec/support/config/vcr.rb
+++ spec/support/config/vcr.rb
@@ -2,5 +2,6 @@ VCR.configure do |config|
   config.cassette_library_dir = "#{ ::Rails.root }/spec/cassettes"
   config.hook_into :webmock
   config.ignore_localhost = true
+  config.filter_sensitive_data('<GOOGLE_API_KEY>') { Settings.google_key }
   config.configure_rspec_metadata!
 end
