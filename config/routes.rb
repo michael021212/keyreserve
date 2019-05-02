23e253c3121bc77867eeb745fe85e105eb13460b
williansaio <williansaio23@gmail.com>
2019-05-02 21:36:34 +0900

    [ADD]利用者のCRUDを追加

--- config/routes.rb
+++ config/routes.rb
@@ -116,6 +116,7 @@ Rails.application.routes.draw do
   namespace :corporation_manage do
     root 'dashboards#index'
     resources :shops, except: :index
+    resources :corporation_users
   end
   # resources :users, only: [:index, :new, :create, :show]
   # resources :plans, except: [:show]
