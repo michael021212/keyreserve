691252aadd6e098c479b553cabe46b32af5a2ae3
williansaio <williansaio23@gmail.com>
2019-05-05 17:54:24 +0900

    [FIX]@shopを定義

--- app/controllers/corporation_manage/facility_temporary_plans_controller.rb
+++ app/controllers/corporation_manage/facility_temporary_plans_controller.rb
@@ -1,4 +1,5 @@
 class CorporationManage::FacilityTemporaryPlansController < CorporationManage::Base
+  before_action :set_shop
   before_action :set_facility
   before_action :set_facility_temporary_plans, only: [:resources, :events]
   before_action :set_facility_temporary_plan, only: [:edit, :update, :destroy]
@@ -15,9 +16,9 @@ class CorporationManage::FacilityTemporaryPlansController < CorporationManage::B
       render :new
     end
   end
-  
+
   def edit; end
-  
+
   def update
     if @facility_temporary_plan.update(facility_temporary_plan_params)
       redirect_to corporation_manage_shop_facility_path(@facility.shop, @facility), notice: "#{FacilityTemporaryPlan.model_name.human}を更新しました。"
@@ -25,30 +26,34 @@ class CorporationManage::FacilityTemporaryPlansController < CorporationManage::B
       render :edit
     end
   end
-  
+
   def destroy
     @facility_temporary_plan.destroy!
     redirect_to corporation_manage_shop_facility_path(@facility.shop, @facility), notice: "#{FacilityTemporaryPlan.model_name.human}を削除しました。"
   end
-  
+
   def resources; end
-  
+
   def events; end
-  
+
   private
-  
-  def set_facility
-    @facility = Facility.belongs_to_corporation(current_corporation).find(params[:facility_id])
+
+  def set_shop
+    @shop = current_corporation.shops.find(params[:shop_id])
   end
-  
+
+  def set_facility
+    @facility = @shop.facilities.find(params[:facility_id])
+  end
+
   def set_facility_temporary_plans
     @facility_temporary_plans = @facility.facility_temporary_plans
   end
-  
+
   def set_facility_temporary_plan
     @facility_temporary_plan = @facility.facility_temporary_plans.find(params[:id])
   end
-  
+
   def facility_temporary_plan_params
     params.require(:facility_temporary_plan).permit(
       :plan_id,
