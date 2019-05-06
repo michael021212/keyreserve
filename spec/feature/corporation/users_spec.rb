85f8a4466ad485db6f549c8bd49f871b298cc8cc
williansaio <williansaio23@gmail.com>
2019-05-06 14:01:58 +0900

    [FIX]不要なインデントの削除

--- spec/feature/corporation/users_spec.rb
+++ spec/feature/corporation/users_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature 'corporation/users', type: :feature do
   let(:corporation_user) { create(:corporation_user,
                                   user: user,
                                   corporation: corporation) }
-  
+
   feature '利用者作成' do
     before do
       corporation
@@ -14,16 +14,16 @@ RSpec.feature 'corporation/users', type: :feature do
       corporation_user
       login_user(user)
     end
-    
+
     scenario '利用者を作成できる' do
       visit corporation_manage_root_path
-      
+
       click_on('利用者管理')
-      
+
       within(find('.box-tools')) do
         click_on('新規追加')
       end
-      
+
       fill_in 'お名前', with: '橋本環奈'
       fill_in 'メールアドレス', with: 'hashimoto@example.com'
       fill_in '電話番号', with: '08012311231'
@@ -31,9 +31,9 @@ RSpec.feature 'corporation/users', type: :feature do
       select 'クレジットカード', from: 'user[payway]'
       fill_in 'パスワード', with: 'Password123'
       fill_in '確認用パスワード', with: 'Password123'
-      
+
       click_on('登録')
-      
+
       expect(find('.alert-warning')).to have_content('利用者を作成しました。')
       expect(find('.cy-user-name')).to have_content('橋本環奈')
       expect(find('.cy-user-email')).to have_content('hashimoto@example.com')
@@ -42,7 +42,7 @@ RSpec.feature 'corporation/users', type: :feature do
       expect(find('.cy-user-payway')).to have_content('クレジットカード')
     end
   end
-  
+
   feature '利用者編集' do
     let(:target_user) { create(:user,
                                name: '吉岡里帆',
@@ -53,7 +53,7 @@ RSpec.feature 'corporation/users', type: :feature do
     let(:corporation_user_2) { create(:corporation_user,
                                       user: target_user,
                                       corporation: corporation)}
-    
+
     before do
       corporation
       user
@@ -62,16 +62,16 @@ RSpec.feature 'corporation/users', type: :feature do
       corporation_user_2
       login_user(user)
     end
-    
+
     scenario '利用者を編集できる' do
       visit corporation_manage_root_path
-      
+
       click_on('利用者管理')
-      
+
       within(find(".cy-user-#{target_user.id}")) do
         click_on('編集')
       end
-      
+
       fill_in 'お名前', with: 'ジョンレノン'
       fill_in 'メールアドレス', with: 'jon@example.com'
       fill_in '電話番号', with: '07011111111'
@@ -79,9 +79,9 @@ RSpec.feature 'corporation/users', type: :feature do
       select '請求書', from: 'user[payway]'
       fill_in 'パスワード', with: 'Password123'
       fill_in '確認用パスワード', with: 'Password123'
-      
+
       click_on('更新')
-      
+
       expect(find('.alert-warning')).to have_content('利用者を更新しました。')
       expect(find('.cy-user-name')).to have_content('ジョンレノン')
       expect(find('.cy-user-email')).to have_content('jon@example.com')
@@ -90,7 +90,7 @@ RSpec.feature 'corporation/users', type: :feature do
       expect(find('.cy-user-payway')).to have_content('請求書')
     end
   end
-  
+
   feature '利用者削除' do
     context 'ログインしているユーザーの場合' do
       before do
@@ -99,24 +99,24 @@ RSpec.feature 'corporation/users', type: :feature do
         corporation_user
         login_user(user)
       end
-      
+
       scenario '削除ボタンが表示されない' do
         visit corporation_manage_root_path
-        
+
         click_on('利用者管理')
-        
+
         within(find(".cy-user-#{user.id}")) do
           expect(page).not_to have_content('削除')
         end
       end
     end
-    
+
     context 'ログインしているユーザー以外の場合' do
       let(:target_user) { create(:user)}
       let(:corporation_user_2) { create(:corporation_user,
                                         user: target_user,
                                         corporation: corporation)}
-      
+
       before do
         corporation
         user
@@ -124,18 +124,18 @@ RSpec.feature 'corporation/users', type: :feature do
         corporation_user_2
         login_user(user)
       end
-      
+
       scenario 'ユーザーを削除できる', js: true do
         visit corporation_manage_root_path
-        
+
         click_on('利用者管理')
-        
+
         within(find(".cy-user-#{target_user.id}")) do
           click_on('削除')
         end
-        
+
         page.driver.browser.switch_to.alert.accept
-        
+
         expect(find('.alert-warning')).to have_content('利用者を削除しました。')
         expect(page).not_to have_css(".cy-user-#{target_user.id}")
       end
