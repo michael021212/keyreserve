4e55e68ed2b1c54e6e2360c17e2ae17242a5d7a5
williansaio <williansaio23@gmail.com>
2019-05-09 10:54:33 +0900

    [FIX]三項演算子の書き方に修正

--- app/decorators/information_decorator.rb
+++ app/decorators/information_decorator.rb
@@ -2,10 +2,6 @@ class InformationDecorator < Draper::Decorator
   delegate_all
   
   def mail_send_flag_to_string
-    if mail_send_flag?
-      '◯'
-    else
-      ''
-    end
+    mail_send_flag? ? '◯' : ''
   end
 end
