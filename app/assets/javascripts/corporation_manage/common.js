2d588daf9c048dd12276ea71349db9fd88413766
williansaio <williansaio23@gmail.com>
2019-05-14 11:57:26 +0900

    [FIX]js周りの修正

--- app/assets/javascripts/corporation_manage/common.js
+++ app/assets/javascripts/corporation_manage/common.js
@@ -1,19 +1,50 @@
-$(document).on('ready', function () {
-  var $this = $('#reservation_block_flag')
-  if ($this.is(':checked')) {
-    $('#reservation_user_id').val('')
-    $('#reservation_user_id').attr('disabled', 'true')
+var dispInfoShops = function () {
+  if($('#information_info_target_type').val() == 'all_users') {
+    $('.js-info-shops').hide()
   } else {
-    $('#reservation_user_id').removeAttr('disabled')
+    $('.js-info-shops').show()
   }
-})
+};
 
-$(document).on('click', '#reservation_block_flag', function () {
-  var $this = $(this)
-  if ($this.is(':checked')) {
-    $('#reservation_user_id').val('')
-    $('#reservation_user_id').attr('disabled', 'true')
+var disabledForm = function (target_class) {
+  $(target_class).val('')
+  $(target_class).attr('disabled', 'true')
+};
+
+var enabledForm = function (target_class) {
+  $(target_class).removeAttr('disabled', 'false')
+};
+
+var hideForm = function (target_class) {
+  $(target_class).hide()
+};
+
+var showForm = function (target_class) {
+  $(target_class).show()
+}
+
+var disabledReservationForms = function (target_class) {
+  if ($(target_class).is(':checked')) {
+    disabledForm('#reservation_user_id');
+    disabledForm('#reservation_num');
+    hideForm('.js-mail-send-flag')
   } else {
-    $('#reservation_user_id').removeAttr('disabled')
+    enabledForm('#reservation_user_id');
+    enabledForm('#reservation_num');
+    showForm('.js-mail-send-flag')
   }
-})
+};
+
+$(document)
+  .on('ready', function () {
+    dispInfoShops();
+    disabledReservationForms('#reservation_block_flag')
+  })
+
+  .on('click', '#reservation_block_flag', function () {
+    disabledReservationForms(this)
+  })
+
+  .on('change', '#information_info_target_type', function () {
+    dispInfoShops();
+  });
