d0d80e2c3d8dc4fc02ee010eb6a84688f32cd275
williansaio <williansaio23@gmail.com>
2019-05-15 15:28:47 +0900

    [ADD]i18n化

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,6 +1,9 @@
 module UserHelper
   
   def select_user_type_without_parent_corporation
-    [['利用者', 'personal'], ['施設管理者', 'corporate_admin']]
+    [
+      [I18n.t('enums.user.user_type.personal'), 'personal'],
+      [I18n.t('enums.user.user_type.corporate_admin'), 'corporate_admin']
+    ]
   end
 end
