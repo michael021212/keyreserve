998786d12dfb81cace2db913e2ce57cb497925ae
NaokiMochizuki <naoki.mochziuki1006@gmail.com>
2019-08-22 13:34:03 +0900

    予約検索が月内最終日も含まれるように修正

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -2,7 +2,7 @@ Ransack.configure do |config|
   config.add_predicate :during_month,
                        arel_predicate: :between,
                        formatter: proc { |v|
-                         v.beginning_of_month.to_date...v.end_of_month.to_date
+                         v.beginning_of_month.beginning_of_day.to_datetime..v.end_of_month.end_of_day.to_datetime
                        },
                        type: :date
 end
