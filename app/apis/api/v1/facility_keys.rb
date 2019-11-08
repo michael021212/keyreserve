a3a2a98c4f375de11d32c9b5c3ae0dc07e98636f
NaokiMochizuki <naoki.mochziuki1006@gmail.com>
2019-11-08 19:12:39 +0900

    部屋鍵の更新APIを簡略化

--- app/apis/api/v1/facility_keys.rb
+++ app/apis/api/v1/facility_keys.rb
@@ -4,15 +4,13 @@ module API
       resource :facility_keys do
         params do
           requires :facility_id, type: Integer
-          requires :ks_previous_room_key_id, type: Integer
           requires :ks_room_key_id, type: Integer
         end
         patch '/', jbuilder: 'facility_keys/update' do
           @facility = Facility.find_by('id = ?', params[:facility_id])
           raise_with_message("facility is not found", 404) if @facility.blank?
           raise_with_message("facility is invalid", 400) if @facility.shop.corporation_id != @corporation.id
-          @facility_key = @facility.facility_keys.find_by('ks_room_key_id = ?',
-                                                          params[:ks_previous_room_key_id])
+          @facility_key = @facility.facility_keys.first
           raise_with_message("facility key is not found", 404) if @facility_key.blank?
           @facility_key.update!(ks_room_key_id: params[:ks_room_key_id])
         end
