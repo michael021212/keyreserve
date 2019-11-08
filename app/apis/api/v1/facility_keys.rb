5d29d0754384e4cabacc7d4d629bd046955b4cf1
NaokiMochizuki <naoki.mochziuki1006@gmail.com>
2019-11-08 13:55:03 +0900

    room_key、createとdestroyのAPI作成

--- app/apis/api/v1/facility_keys.rb
+++ app/apis/api/v1/facility_keys.rb
@@ -16,6 +16,31 @@ module API
           raise_with_message("facility key is not found", 404) if @facility_key.blank?
           @facility_key.update!(ks_room_key_id: params[:ks_room_key_id])
         end
+
+        namespace do
+          params do
+            requires :facility_id, type: Integer
+            requires :ks_room_key_id, type: Integer
+          end
+
+          post '/', jbuilder: 'facility_keys/create' do
+            @facility = Facility.find_by('id = ?', params[:facility_id])
+            raise_with_message("facility is not found", 404) if @facility.blank?
+            raise_with_message("facility is invalid", 400) if @facility.shop.corporation_id != @corporation.id
+            @facility_key = @facility.facility_keys.create!(ks_room_key_id: params[:ks_room_key_id],
+                                                            name: "#{@facility.name}")
+          end
+        end
+
+        # FacilityIDから紐づくkeyを全て削除するAPI
+        namespace do
+          delete '/:facility_id', jbuilder: 'facility_keys/destroy' do
+            @facility = Facility.find_by('id = ?', params[:facility_id])
+            raise_with_message("facility is not found", 404) if @facility.blank?
+            raise_with_message("facility is invalid", 400) if @facility.shop.corporation_id != @corporation.id
+            @facility_key = @facility.facility_keys.destroy_all
+          end
+        end
       end
     end
   end
