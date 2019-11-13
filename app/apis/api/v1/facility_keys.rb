3d125d2a834e0765f32dcb71c2e38e1bba52d751
NaokiMochizuki <naoki.mochziuki1006@gmail.com>
2019-11-13 18:38:40 +0900

    リファクタ

--- app/apis/api/v1/facility_keys.rb
+++ app/apis/api/v1/facility_keys.rb
@@ -14,12 +14,9 @@ module API
           raise_with_message("facility is invalid", 400) if @facility.shop.corporation_id != @corporation.id
           @facility_key = @facility.facility_keys.first
           raise_with_message("facility key is not found", 404) if @facility_key.blank?
-          @facility_key.update!(ks_room_key_id: params[:ks_room_key_id])
-          # KSCの予約から鍵IDの削除
-          if @facility_key.facility.rent_with_ksc?
-            @facility_key.facility.ksc_reservations_after_today.each do |rsv|
-              rsv.update_ksc_reservation
-            end
+          ActiveRecord::Base.transaction do
+            @facility.ksc_reservations_after_today.each { |rsv| rsv.update_ksc_reservation } if @facility.rent_with_ksc?
+            @facility_key.update!(ks_room_key_id: params[:ks_room_key_id])
           end
         end
 
@@ -34,15 +31,10 @@ module API
             @facility = Facility.find_by('id = ?', params[:facility_id])
             raise_with_message("facility is not found", 404) if @facility.blank?
             raise_with_message("facility is invalid", 400) if @facility.shop.corporation_id != @corporation.id
-              @facility_key = @facility
-                              .facility_keys
-                              .create!(ks_room_key_id: params[:ks_room_key_id],
-                                       name: "#{@facility.name}")
-            # KSCの予約から鍵IDの削除
-            if @facility_key.facility.rent_with_ksc?
-              @facility_key.facility.ksc_reservations_after_today.each do |rsv|
-                rsv.update_ksc_reservation
-              end
+            ActiveRecord::Base.transaction do
+              @facility.ksc_reservations_after_today.each { |rsv| rsv.update_ksc_reservation } if @facility.rent_with_ksc?
+              @facility_key = @facility.facility_keys.create!(ks_room_key_id: params[:ks_room_key_id],
+                                                              name: "#{@facility.name}")
             end
           end
         end
@@ -57,12 +49,9 @@ module API
             @facility = Facility.find_by('id = ?', params[:facility_id])
             raise_with_message("facility is not found", 404) if @facility.blank?
             raise_with_message("facility is invalid", 400) if @facility.shop.corporation_id != @corporation.id
-            @facility.facility_keys.destroy_all
-            # KSCの予約から鍵IDの削除
-            if @facility.rent_with_ksc?
-              @facility.ksc_reservations_after_today.each do |rsv|
-                rsv.update_ksc_reservation
-              end
+            ActiveRecord::Base.transaction do
+              @facility.ksc_reservations_after_today.each { |rsv| rsv.update_ksc_reservation } if @facility.rent_with_ksc?
+              @facility.facility_keys.destroy_all
             end
           end
         end
