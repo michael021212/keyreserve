3a80933b16e6b00121c00b0e204f7eb731422a42
NaokiMochizuki <naoki.mochziuki1006@gmail.com>
2020-02-19 11:39:06 +0900

    ブロック予約作成用のAPI作成

---
+++ app/apis/api/v1/reservations.rb
@@ -0,0 +1,39 @@
+module API
+  module V1
+    class Reservations < Grape::API
+      resource :reservations do
+        # ブロック予約の作成
+        params do
+          requires :facility_id, type: Integer
+          requires :checkin, type: Integer
+          requires :checkout, type: Integer
+        end
+        post '/block', jbuilder: 'reservations/block' do
+          @facility = Facility.find_by('id=?', params[:facility_id])
+          checkin = Time.at(params[:checkin])
+          checkout = Time.at(params[:checkout])
+          usage_period = (checkout - checkin)/60/60
+          raise_with_message("facility is not found", 404) if @facility.blank?
+          raise_with_message("facility is invalid", 400) if @facility.shop.corporation != @corporation
+          begin
+            ActiveRecord::Base.transaction do
+              @reservation = Reservation.new(
+                facility_id: @facility.id,
+                checkin: checkin,
+                checkout: checkout,
+                usage_period: usage_period,
+                state: Reservation.states[:confirmed],
+                block_flag: true,
+                price: 0
+              )
+              @reservation.save!
+            end
+          rescue => e
+            @error = e.to_s
+          end
+        end
+      end
+    end
+  end
+end
+
--- app/apis/api/v1/root.rb
+++ app/apis/api/v1/root.rb
@@ -75,6 +75,7 @@ module API
       mount API::V1::FacilityKeys
       mount API::V1::Users
       mount API::V1::Sessions
+      mount API::V1::Reservations
     end
   end
 end
