9180f3689ec4c793d1b42d981774870cf7f97240
tanakashun <michael021212@gmail.com>
2021-06-22 13:31:00 +0900

    deproy.rb編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,8 +27,9 @@ set :linked_dirs, %w(log tmp/backup tmp/pids tmp/cache tmp/sockets vendor/bundle
 # Default value for :linked_files is []
 set :linked_files, %w(.env config/database.yml)
 set :rbenv_type, :user
-set :rbenv_path, '/usr/local/rbenv'
-set :rbenv_ruby, File.read('.ruby-version').strip
+# set :rbenv_path, '/usr/local/rbenv'
+# set :rbenv_ruby, File.read('.ruby-version').strip
+set :rbenv_ruby, '2.5.7'
 set :rbenv_prefix, "WEBPACKER_COMPILE=false RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_roles, :all
 
